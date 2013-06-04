f580076d39c567c227e3ff69a6e90e5d54d72a01
se-som <se.somitc41@gmail.com>
2013-06-04 08:31:29 -0700

    add new module

---
+++ config/application.config.php.REMOTE.php
@@ -0,0 +1,19 @@
+<?php
+return array(
+    'modules' => array(
+	'DOMPDFModule',
+		'DoctrineModule',
+		'DoctrineORMModule',
+		'KJ',
+		'User',
+    ),
+    'module_listener_options' => array(
+        'config_glob_paths'    => array(
+            'config/autoload/{,*.}{global,local}.php',
+        ),
+        'module_paths' => array(
+            './module',
+            './vendor',
+        ),
+    ),
+);
